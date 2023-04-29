c2cb97f61ef0a768b3f26985c99cf8af92626e10
omkar-here <aomkarg2003@gmail.com>
2023-04-30 02:11:06 +0530

    Ready for changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Registration from "./Components/Registration";
+import Login from "./Components/Login";
+import MainPage from "./Components/MainPage";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  // Link,
+  // NavLink,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Components/Layout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="registration" element={<Registration />} />
+      <Route path="login" element={<Login />} />
+    </Route>
+  )
+);
 
 function App() {
-
   return (
     <>
-      <h1 className='text-6xl'>Hello world</h1>
+      {/* <Registration /> */}
+      {/* <Login /> */}
+      {/* <MainPage /> */}
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
